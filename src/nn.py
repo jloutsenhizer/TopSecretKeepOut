554b6983a8967fdbf93480fa9e54a29c93914515
Mario Minyon <mariominyon@gmail.com>
2014-04-12 19:51:30 -0400

    Update nn.py
    added some runner code

--- src/nn.py
+++ src/nn.py
@@ -7,16 +7,15 @@ from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import LinearLayer, SigmoidLayer, SoftmaxLayer
 from pybrain.structure import FullConnection
 from SmartPlayer import SmartPlayer
+from RandomPlayer import RandomPlayer
+from Othello import Othello
+import random
+
+
 
 #sets a ClassificationDataSet with 16 inputs and 10 outputs
 ds = ClassificationDataSet(192,64,nb_classes=64)
 ds = GETDATAFROMSMARTPLAYER()
-
-tstdata, trndata = ds.splitWithProportion( 0.25 )
-
-trndata._convertToOneOfMany( )
-tstdata._convertToOneOfMany( )
-
 
 #create network with no hidden layers
 nn = FeedForwardNetwork()
@@ -27,5 +26,27 @@ nn.addOutputModule(outLayer)
 nn.addConnection(FullConnection(inLayer, outLayer))
 nn.sortModules()
 
-trainer = BackpropTrainer(n, dataset=trndata)
-trainer.trainUntilConvergence(maxEpochs=100)
+othello = Othello()
+smartPlayer = SmartPlayer(nn,othello.boardSize)
+randomPlayer = RandomPlayer()
+randomPlayer.newGame(othello,random.choice([othello.WHITE_PLAYER,othello.BLACK_PLAYER]));
+smartPlayer.newGame(othello,randomPlayer.enemy);
+
+while not othello.isGameOver():
+	if (othello.whoGoesNext() == randomPlayer.color):
+		move = randomPlayer.getMove()
+		othello.makeMove(move[0],move[1],randomPlayer.color)
+	else:
+		move = smartPlayer.getMove()
+		othello.makeMove(move[0],move[1],smartPlayer.color)
+if othello.getWinner() == randomPlayer.color:
+	outcome = -1
+else if othello.getWinner() == smartPlayer.color:
+	outcome = 1
+else
+	outcome = 0
+
+ds = smartPlayer.gameOver(outcome)
+if (ds != None):
+	trainer = BackpropTrainer(nn, dataset=ds)
+	trainer.trainUntilConvergence(maxEpochs=100)
