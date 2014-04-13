62db9a673cc10dfbe5e0ed6c6b9e8e0a5c026517
Mario Minyon <mariominyon@gmail.com>
2014-04-12 21:11:16 -0400

    reads network, plays a game, trains the network, then writes the network out to "othelloNetwork.xml"

--- src/nn.py
+++ src/nn.py
@@ -1,7 +1,9 @@
 #Mario Minyon
 #CS 1675
 #HW 3
-from pybrain.datasets            import ClassificationDataSet
+from pybrain.tools.customxml.networkwriter import NetworkWriter
+from pybrain.tools.customxml.networkreader import NetworkReader
+from pybrain.datasets import ClassificationDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import LinearLayer, SigmoidLayer, SoftmaxLayer
@@ -9,44 +11,65 @@ from pybrain.structure import FullConnection
 from SmartPlayer import SmartPlayer
 from RandomPlayer import RandomPlayer
 from Othello import Othello
+from Player import playGame
+import sys
 import random
+import os.path
+import time
 
+count = 0
+while (count < 100):
+	#sets a ClassificationDataSet with 16 inputs and 10 outputs
+	ds = ClassificationDataSet(192,64,nb_classes=64)
 
+	#create network with no hidden layers
+	nn = FeedForwardNetwork()
+	#checks to see if there is already a network created
+	if os.path.isfile("othelloNetwork.xml"):
+		nn =  NetworkReader.readFrom("othelloNetwork.xml")
 
-#sets a ClassificationDataSet with 16 inputs and 10 outputs
-ds = ClassificationDataSet(192,64,nb_classes=64)
-ds = GETDATAFROMSMARTPLAYER()
-
-#create network with no hidden layers
-nn = FeedForwardNetwork()
-inLayer = LinearLayer(192)
-outLayer = SoftmaxLayer(64)
-nn.addInputModule(inLayer)
-nn.addOutputModule(outLayer)
-nn.addConnection(FullConnection(inLayer, outLayer))
-nn.sortModules()
-
-othello = Othello()
-smartPlayer = SmartPlayer(nn,othello.boardSize)
-randomPlayer = RandomPlayer()
-randomPlayer.newGame(othello,random.choice([othello.WHITE_PLAYER,othello.BLACK_PLAYER]));
-smartPlayer.newGame(othello,randomPlayer.enemy);
-
-while not othello.isGameOver():
-	if (othello.whoGoesNext() == randomPlayer.color):
-		move = randomPlayer.getMove()
-		othello.makeMove(move[0],move[1],randomPlayer.color)
 	else:
-		move = smartPlayer.getMove()
-		othello.makeMove(move[0],move[1],smartPlayer.color)
-if othello.getWinner() == randomPlayer.color:
-	outcome = -1
-else if othello.getWinner() == smartPlayer.color:
-	outcome = 1
-else
-	outcome = 0
+		inLayer = LinearLayer(192)
+		hiddenLayer1 = SigmoidLayer(128)
+		hiddenLayer2 = SoftmaxLayer(80)
+		hiddenLayer3 = SigmoidLayer(33)
+		outLayer = SoftmaxLayer(64)
+		nn.addInputModule(inLayer)
+		nn.addOutputModule(outLayer)
+		nn.addModule(hiddenLayer1)
+		nn.addModule(hiddenLayer2)
+		nn.addModule(hiddenLayer3)
+		nn.addConnection(FullConnection(inLayer, hiddenLayer1))
+		nn.addConnection(FullConnection(inLayer, hiddenLayer2))
+		nn.addConnection(FullConnection(hiddenLayer2, hiddenLayer3))
+		nn.addConnection(FullConnection(hiddenLayer1, outLayer))
+		nn.addConnection(FullConnection(hiddenLayer3, outLayer))
+		nn.sortModules()
 
-ds = smartPlayer.gameOver(outcome)
-if (ds != None):
-	trainer = BackpropTrainer(nn, dataset=ds)
-	trainer.trainUntilConvergence(maxEpochs=100)
+	othello = Othello()
+	smartPlayer = SmartPlayer(nn,othello.boardSize)
+	randomPlayer = RandomPlayer()
+	randomPlayer.newGame(othello,random.choice([othello.WHITE_PLAYER,othello.BLACK_PLAYER]));
+	smartPlayer.newGame(othello,randomPlayer.enemy);
+
+	playGame(othello,randomPlayer,smartPlayer)
+	if othello.getWinner() == randomPlayer.color:
+		outcome = -1
+		print "Random Player wins over Smart Player"
+	elif othello.getWinner() == smartPlayer.color:
+		outcome = 1
+		print "Smart Player wins over Random Player"	
+	else:
+		outcome = 0
+		print "Tie Game"
+
+	ds = smartPlayer.gameOver(outcome)
+	if (ds != None):
+		trainer = BackpropTrainer(nn, dataset=ds)
+		trainer.trainUntilConvergence(maxEpochs=100)
+		NetworkWriter.writeToFile(nn, "othelloNetwork.xml")
+		print "Terminate now to safely save"
+		time.sleep(3)
+		print "TOO LATE"
+	
+	count += 1
