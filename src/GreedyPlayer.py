b0aab887083760787892458baabe8bba414d178c
equarles <equarles1@gmail.com>
2014-04-14 14:14:28 -0400

    GreedyPlayer
    Code for the greedy AI

---
+++ src/GreedyPlayer.py
@@ -0,0 +1,28 @@
+from pybrain.structure import FeedForwardNetwork
+from pybrain.datasets import SupervisedDataSet
+from Othello import Othello
+import random
+
+class GreedyPlayer: 	
+    def newGame(self, game, color):
+        self.color = color
+        self.game = game;
+        if color == self.game.WHITE_PLAYER:
+            self.enemy = self.game.BLACK_PLAYER
+        else:
+            self.enemy = self.game.WHITE_PLAYER
+
+    def getMove(self):  #returns the network's choice for "best" move
+        max = 0
+        move = (-1,-1)
+        options = self.game.getAllPossibleMoves(self.color)
+        
+        for i in range(len(options)):
+            x, y = options[i]
+            val = getPointsofChange(x, y, self.color)
+            
+            if val > max
+                max = val
+                move = (x, y)
+                
+        return move
